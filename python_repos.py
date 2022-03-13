35e2ecf4ef21943c3fe58d3ba884a90bc8bc3108
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-03-12 23:14:22 -0500

    Create python_repos.py

---
+++ python_repos.py
@@ -0,0 +1,41 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print ("Status Code:", r.status_code)
+
+response_dict = r.json()
+print ("Total repositories:", response_dict['total_count'])
+repo_dicts = response_dict['items']
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    description = repo_dict['description']
+    if not description:
+        description = "No description provided"
+
+    plot_dict = {'value': repo_dict['stargazers_count'],
+                 'label': description,
+                 'xlink': repo_dict['html_url']}
+    plot_dicts.append(plot_dict)
+
+my_style = LS('#333366', basestyle = LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
